d5f16fb702ac0efab3f9bef8f2f76f840d3e90fd
RawiSader <rawinsader@gmail.com>
2022-02-05 18:21:54 -0500

    feat: implemented the transfer function and its tests

--- contracts/Netvrk.sol
+++ contracts/Netvrk.sol
@@ -11,13 +11,25 @@ contract Netvrk {
 
     mapping(address => uint256) public balanceOf;
 
-    uint256 public totalSupply;
-
-    address private owner;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
     // Setting the total amount of tokens available
+    // Setting the owner of the contract
+    // Assigning the total supply to the owner of the contract
     constructor(uint256 _initialSupply) public {
         totalSupply = _initialSupply;
         owner = msg.sender;
         balanceOf[owner] = _initialSupply;
+    }
+
+    // Transferring tokens ownership from an account to the other
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient funds.");
+        require(balanceOf[_to] + _value > balanceOf[_to], "Integer overflow.");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/Netvrk.js
+++ test/Netvrk.js
@@ -26,4 +26,31 @@ contract('Netvrk', (accounts) => {
     const balance = await instance.balanceOf(accounts[0]);
     assert.equal(balance, TOTAL_SUPPLY, 'Error: contract owner does not initially own all tokens.');
   });
+  it('Detect insufficient funds when transferring tokens', async () => {
+    const instance = await Netvrk.deployed();
+    try {
+      await instance.transfer.call(accounts[1], 1234123412341234);
+    } catch(error) {
+      assert(error.message.indexOf('Insufficient funds.') >= 0, 'Error: Insufficient funds not detected.');
+    }
+  });
+  it('Transferring tokens', async () => {
+    const instance = await Netvrk.deployed();
+    const receipt = await instance.transfer(accounts[1], 100, { from: accounts[0] });
+
+    const senderBalance = await instance.balanceOf(accounts[0]);
+    const recipientBalance = await instance.balanceOf(accounts[1]);
+    assert.equal(senderBalance, TOTAL_SUPPLY - 100, 'Error: Failed to reduce the sender\'s balance by the right amount.');
+    assert.equal(recipientBalance, 100, 'Error: failed to increase the recipient\'s balance by the right amount.');
+
+    assert.equal(receipt.logs.length, 1, 'Error: no events triggered.');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'Error: event should be a Transfer event.');
+    assert.equal(receipt.logs[0].args._from, accounts[0], 'Error: sender is not specified in the event.');
+    assert.equal(receipt.logs[0].args._to, accounts[1], 'Error: recipient is not specified in the event.');
+    assert.equal(receipt.logs[0].args._value, 100, 'Error: value is not specified in the event.');
+
+    const transferred = await instance.transfer.call(accounts[1], 100, { from: accounts[0] });
+    assert.equal(transferred, true, 'Error: function does not return a boolean.');
+
+  });
 });
