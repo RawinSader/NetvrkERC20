caac94b42b8317b71276b305b05c0668e1216eec
RawiSader <rawinsader@gmail.com>
2022-02-05 17:10:14 -0500

    added and separated deployment tests

--- test/Netvrk.js
+++ test/Netvrk.js
@@ -1,10 +1,19 @@
 const Netvrk = artifacts.require('Netvrk');
-const { TOTAL_SUPPLY } = require('../common/constants');
+const { TOTAL_SUPPLY, ZERO_ADDRESS } = require('../common/constants');
 
 contract('Netvrk', (accounts) => {
-  it('properly sets total supply when deployed', async () => {
+  it('Deploy the contract', async () => {
+    const instance = await Netvrk.deployed();
+    assert.notEqual(instance.address, ZERO_ADDRESS, "Contract has address")
+  });
+  it('Set the total supply on deployment', async () => {
     const instance = await Netvrk.deployed();
     const totalSupply = await instance.totalSupply();
-    assert.equal(totalSupply.toNumber(), TOTAL_SUPPLY, "Total supply was set to correctly")
+    assert.equal(totalSupply.toNumber(), TOTAL_SUPPLY, 'Total supply was set to correctly');
+  });
+  it('Assign all initial tokens to the contract owner on deployment', async () => {
+    const instance = await Netvrk.deployed();
+    const balance = await instance.balanceOf(accounts[0]);
+    assert.equal(balance, TOTAL_SUPPLY, 'Contract owner initially owns all of the tokens');
   });
 });
