9ce823222bb2c357013fcb30928f9483d85c0eb0
RawiSader <rawinsader@gmail.com>
2022-02-05 19:40:44 -0500

    feat: implemented transferFrom functionality

--- contracts/Netvrk.sol
+++ contracts/Netvrk.sol
@@ -10,8 +10,10 @@ contract Netvrk {
     address private owner;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     // Setting the total amount of tokens available
     // Setting the owner of the contract
@@ -31,6 +33,28 @@ contract Netvrk {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, "Insufficient funds.");
+        require(balanceOf[_to] + _value > balanceOf[_to], "Integer overflow.");
+        require(allowance[_from][msg.sender] >= _value, "Cannot exceed allowance.");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 }
--- test/Netvrk.js
+++ test/Netvrk.js
@@ -45,12 +45,71 @@ contract('Netvrk', (accounts) => {
 
     assert.equal(receipt.logs.length, 1, 'Error: no events triggered.');
     assert.equal(receipt.logs[0].event, 'Transfer', 'Error: event should be a Transfer event.');
-    assert.equal(receipt.logs[0].args._from, accounts[0], 'Error: sender is not specified in the event.');
-    assert.equal(receipt.logs[0].args._to, accounts[1], 'Error: recipient is not specified in the event.');
-    assert.equal(receipt.logs[0].args._value, 100, 'Error: value is not specified in the event.');
+    assert.equal(receipt.logs[0].args._from, accounts[0], 'Error: wrong sender specified in the event.');
+    assert.equal(receipt.logs[0].args._to, accounts[1], 'Error: wrong recipient specified in the event.');
+    assert.equal(receipt.logs[0].args._value, 100, 'Error: wrong value specified in the event.');
 
     const transferred = await instance.transfer.call(accounts[1], 100, { from: accounts[0] });
-    assert.equal(transferred, true, 'Error: function does not return a boolean.');
+    assert.equal(transferred, true, 'Error: Failed to transfer tokens.');
+
+  });
+  it('Approve tokens', async () => {
+    const instance = await Netvrk.deployed();
+
+    const receipt = await instance.approve(accounts[1], 100);
+
+    const allowance = await instance.allowance(accounts[0], accounts[1]);
+    assert.equal(allowance.toNumber(), 100, 'Error: Allowance not set properly.');
+
+    assert.equal(receipt.logs.length, 1, 'Error: no events triggered.');
+    assert.equal(receipt.logs[0].event, 'Approval', 'Error: event should be an Approval event.');
+    assert.equal(receipt.logs[0].args._owner, accounts[0], 'Error: wrong owner specified in the event.');
+    assert.equal(receipt.logs[0].args._spender, accounts[1], 'Error: wrong spender specified in the event.');
+    assert.equal(receipt.logs[0].args._value, 100, 'Error: wrong value specified in the event.');
+
+    const approved = await instance.approve.call(accounts[1], 100);
+    assert.equal(approved, true, 'Error: Failed to approve tokens.');
+  });
+  it('Transfer tokens from another account', async () => {
+    const instance = await Netvrk.deployed();
+
+    from = accounts[2];
+    to = accounts[3];
+    spender = accounts[4];
+
+    await instance.transfer(from, 100, { from: accounts[0] });
+    await instance.approve(spender, 20, { from });
+
+    try {
+      await instance.transferFrom(from, to, 12341234, { from: spender });
+    } catch(error) {
+      assert(error.message.indexOf('Insufficient funds.') >= 0, 'Error: Insufficient funds not detected.');
+    }
+    try {
+      await instance.transferFrom(from, to, 30, { from: spender });
+    } catch(error) {
+      assert(error.message.indexOf('Cannot exceed allowance.') >= 0, 'Error: spending more not detected.');
+    }
+
+    const receipt = await instance.transferFrom(from, to, 10, { from: spender });
+
+    const fromBalance = await instance.balanceOf(from);
+    const toBalance = await instance.balanceOf(to);
+
+    assert.equal(fromBalance, 100 - 10, 'Error: from balance not decreased properly.');
+    assert.equal(toBalance, 10, 'Error: to balance not increased properly.');
+
+    const allowance = await instance.allowance(from, spender);
+    assert.equal(allowance.toNumber(), 20 - 10, 'Error: allowance not decreased properly.');
+
+    assert.equal(receipt.logs.length, 1, 'Error: no events triggered.');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'Error: event should be a Transfer event.');
+    assert.equal(receipt.logs[0].args._from, from, 'Error: wrong sender specified in the event.');
+    assert.equal(receipt.logs[0].args._to, to, 'Error: wrong recipient specified in the event.');
+    assert.equal(receipt.logs[0].args._value, 10, 'Error: wrong value specified in the event.');
+
+    const transferredFrom = await instance.transferFrom.call(from, to, 10, { from: spender });
+    assert.equal(transferredFrom, true, 'Error: Failed to transferFrom tokens');
 
   });
 });
