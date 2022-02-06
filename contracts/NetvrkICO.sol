c0cbbda11ae6aa97befd961b5dc27ec05468234f
RawiSader <rawinsader@gmail.com>
2022-02-06 11:47:22 -0500

    feat: added buy functionality with tests

--- contracts/NetvrkICO.sol
+++ contracts/NetvrkICO.sol
@@ -8,10 +8,28 @@ contract NetvrkICO {
     Netvrk public netvrk;
 
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Purchased(address _buyer, uint256 _amount);
 
     constructor(Netvrk _token, uint256 _tokenPrice) public {
         owner = msg.sender;
         netvrk = _token;
         tokenPrice = _tokenPrice;
+    }
+
+    function buy(uint256 _amount) public payable {
+        require(msg.value == safeMultiply(_amount, tokenPrice), "Payment error.");
+        require(netvrk.balanceOf(address(this)) >= _amount, 'Insufficient funds.');
+        require(tokensSold + _amount > tokensSold, "Integer overflow.");
+        require(netvrk.transfer(msg.sender, _amount), "Transfer failed.");
+
+        tokensSold += _amount;
+        emit Purchased(msg.sender, _amount);
+    }
+
+    function safeMultiply(uint256 x, uint256 y) internal pure returns (uint z){
+        require(y == 0 || (z = x * y) / y == x);
+        return x * y;
     }
 }
--- test/NetvrkICO.js
+++ test/NetvrkICO.js
@@ -1,7 +1,8 @@
 const NetvrkICO = artifacts.require('NetvrkICO');
+const Netvrk = artifacts.require('Netvrk');
 const { ZERO_ADDRESS, NETVRK_TOKEN_PRICE } = require('../common/constants');
 
-contract('NetvrkICO', () => {
+contract('NetvrkICO', (accounts) => {
 
   it('Deploy the contract', async () => {
     const instance = await NetvrkICO.deployed();
@@ -17,6 +18,47 @@ contract('NetvrkICO', () => {
   it('Check token price', async () => {
     const instance = await NetvrkICO.deployed();
     const price = await instance.tokenPrice();
-    assert.equal(price, NETVRK_TOKEN_PRICE, "Error: token price is incorrect.")
+    assert.equal(price, NETVRK_TOKEN_PRICE, 'Error: token price is incorrect.');
+  });
+
+  it('Buy tokens', async () => {
+    const instance = await NetvrkICO.deployed();
+    const netvrkInstance = await Netvrk.deployed();
+    const amount = 3;
+    const icoInitialBalance = 50000;
+
+    const netvrkOwner = accounts[0];
+    const user = accounts[4];
+
+    await netvrkInstance.transfer(instance.address, icoInitialBalance, { from: netvrkOwner });
+
+    const receipt = await instance.buy(amount, { from: user, value: amount * NETVRK_TOKEN_PRICE });
+
+    assert.equal(receipt.logs.length, 1, 'Error: no events triggered.');
+    assert.equal(receipt.logs[0].event, 'Purchased', 'Error: event should be a Purchased event.');
+    assert.equal(receipt.logs[0].args._buyer, user, 'Error: wrong buyer specified in the event.');
+    assert.equal(receipt.logs[0].args._amount.toNumber(), amount, 'Error: wrong amount specified in the event.');
+
+    const totalSold = await instance.tokensSold();
+    assert.equal(totalSold.toNumber(), amount, 'Error: Number of tokens sold not computed properly.');
+
+    const icoBalance = await netvrkInstance.balanceOf(instance.address);
+    assert.equal(icoBalance.toNumber(), icoInitialBalance - amount, 'Error: ICO balance not updated properly.');
+
+    const userBalance = await netvrkInstance.balanceOf(user);
+    assert.equal(userBalance.toNumber(), amount, 'Error: buyer balance not updated properly.');
+
+    try {
+      await instance.buy(amount, { from: user, value: 1 });
+    } catch(error) {
+      assert(error.message.indexOf('Payment error.') >= 0, 'Error: user is not paying the right amount');
+    }
+    try {
+      const moreThanAvailable = 70000;
+      await instance.buy(70000, { from: accounts[1], value: moreThanAvailable * NETVRK_TOKEN_PRICE });
+    } catch(error) {
+      assert(error.message.indexOf('Insufficient funds.') >= 0, 'Error: ICO does not have that many tokens for you to buy.');
+    }
+
   });
 });
